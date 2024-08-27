f39fa1d8b0ceebd929fe359e54d4e8346ba1ab61
BEmerson <b.emerson@ucc.ie>
2024-08-27 12:26:32 +0100

    Initial Version

---
+++ ch07/filestuff.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 17 15:07:05 2019
+
+@author: billemerson
+"""
+with open("myfile.txt", "r") as theFile:
+    allOfIt = theFile.read()
+    print(allOfIt)
+    
